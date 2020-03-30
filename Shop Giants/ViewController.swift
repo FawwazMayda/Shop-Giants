ad2764ad3e6949d54e7a2db7481689cb0ab7a421
David Gunawan <david_gunawan_@hotmail.com>
2020-03-30 09:41:40 +0700

    first commit

---
+++ Shop Giants/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Shop Giants
+//
+//  Created by David Gunawan on 30/03/20.
+//  Copyright © 2020 David Gunawan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
