98d6429f3d80034d4745effc2d874bc1350b839a
fawwazmayda <maydafawwaz@gmail.com>
2020-04-03 16:51:30 +0800

    showing the image already

--- Shop Giants/ViewController.swift
+++ Shop Giants/ViewController.swift
@@ -16,7 +16,11 @@ struct Category {
 }
 class ViewController: UITableViewController {
 
-    var categories = [Category]()
+    var categories = [
+        Category(image: "lessons-1", name: "Aksesori", desc: "Ya Aksesori"),
+         Category(image: "brass-1", name: "Aksesori", desc: "Ya Aksesori"),
+          Category(image: "piano-1", name: "Aksesori", desc: "Ya Aksesori")
+    ]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -24,16 +28,18 @@ class ViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 2
+        return categories.count
     }
     
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 2
+        return 1
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath) as UITableViewCell
-        cell.textLabel?.text = "Section: \(indexPath.section) Row:\(indexPath.row)"
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
+        cell.detailTextLabel?.text = categories[indexPath.row].name
+        cell.imageView?.image = UIImage(named: categories[indexPath.row].image)
+            cell.textLabel?.text = categories[indexPath.row].desc
         return cell
     }
     
